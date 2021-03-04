3c11ae36e0ac748c1d8b872033e08ed1a741b893
Vineet Rathor <35703327+THE-VR7@users.noreply.github.com>
2021-03-04 11:13:19 +0530

    tasks file

--- Backend/Xmeme/Xmeme/Controllers/UserController.cs
+++ Backend/Xmeme/Xmeme/Controllers/UserController.cs
@@ -10,9 +10,7 @@ namespace Xmeme.Controllers
 
     public class UserController : Controller
     {
-        
-}public IActionResult Index()
-        {
-            return View();
-        }
+
     }
+}
+    
